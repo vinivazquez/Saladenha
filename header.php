ba23c84cbf058b651a80d412ee349bae7b79b40a
Igor <igorhenriqueconstant@gmail.com>
2020-03-24 17:04:28 -0300

    Inclusão menu mobile

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
-<header>
+<header class="d-none d-xl-block">
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container p-3">
             <a class="navbar-brand" href="#"><img src="assets/images/logo.png" alt="logo"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <i class="fas fa-bars"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
@@ -24,5 +24,43 @@
         </div>
     </nav>
 </header>
+
+
+<header class="d-xl-none">
+    <div class="container p-4">
+        <a class="navbar-brand" href="#"><img src="assets/images/logo.png" alt="logo"></a>
+        <button type="button" class="navbar-toggler float-right" data-toggle="modal" data-target="#exampleModalCenter">
+            <i class="fas fa-bars"></i>
+        </button>
+    </div>
+</header>
+
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Ínicio <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cardápio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Institucional</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include('pages/partials/social.php') ?>
