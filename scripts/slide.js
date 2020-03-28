ab796d360d2cd414f04a9f11bf853af5c74a3850
Igor <igorhenriqueconstant@gmail.com>
2020-03-28 19:01:39 -0300

    Alteração do Script Slick

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,62 +1,30 @@
-$('.slick')
-    .on('init', () => {
-        $('.slick-slide[data-slick-index="-2"]').addClass('lt2');
-        $('.slick-slide[data-slick-index="-1"]').addClass('lt1');
-        $('.slick-slide[data-slick-index="1"]').addClass('gt1');
-        $('.slick-slide[data-slick-index="2"]').addClass('gt2');
-    })
 
-    .slick({
-        centerMode: true,
-        centerPadding: 0,
-        slidesToShow: 5,
-        slidesToScroll: 3,
-        infinite: true,
-        responsive: [
-            {
-                breakpoint: 900,
-                settings: {
-                    slidesToShow: 1,
-                    infinite: true,
-                }
+$('.slick').slick({
+    centerMode: true,
+    slidesToShow: 5,
+    centerPadding: '0px',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: true,
+                centerMode: true,
+                centerPadding: '0px',
+                slidesToShow: 3
             }
-        ]
-
-
-    }).on('beforeChange', (event, slick, current, next) => {
-        $('.slick-slide.gt2').removeClass('gt2');
-        $('.slick-slide.gt1').removeClass('gt1');
-        $('.slick-slide.lt1').removeClass('lt1');
-        $('.slick-slide.lt2').removeClass('lt2');
-
-        const lt2 = (current < next && current > 0) ? current - 1 : next - 2;
-        const lt1 = (current < next && current > 0) ? current : next - 1;
-        const gt1 = (current < next || next === 0) ? next + 1 : current;
-        const gt2 = (current < next || next === 0) ? next + 2 : current + 1;
-
-        $(`.slick-slide[data-slick-index="${lt2}"]`).addClass('lt2');
-        $(`.slick-slide[data-slick-index="${lt1}"]`).addClass('lt1');
-        $(`.slick-slide[data-slick-index="${gt1}"]`).addClass('gt1');
-        $(`.slick-slide[data-slick-index="${gt2}"]`).addClass('gt2');
-
-        // Clone processing when moving from 5 to 0
-        if (current === 5 && next === 0) {
-            $(`.slick-slide[data-slick-index="${current - 1}"]`).addClass('lt2');
-            $(`.slick-slide[data-slick-index="${current}"]`).addClass('lt1');
-            $(`.slick-slide[data-slick-index="${current + 2}"]`).addClass('gt1');
-            $(`.slick-slide[data-slick-index="${current + 3}"]`).addClass('gt2');
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: true,
+                centerMode: true,
+                centerPadding: '0px',
+                slidesToShow: 1
+            }
         }
-
-        // Clone processing when moving from 0 to 5
-        if (current === 0 && next === 5) {
-            $(`.slick-slide[data-slick-index="${current - 1}"]`).addClass('gt2');
-            $(`.slick-slide[data-slick-index="${current}"]`).addClass('gt1');
-            $(`.slick-slide[data-slick-index="${current - 2}"]`).addClass('lt1');
-            $(`.slick-slide[data-slick-index="${current - 3}"]`).addClass('lt2');
-        }
-
-        console.log('beforeChange', current, ':', lt2, lt1, next, gt1, gt2);
-
-    });
-
-
+    ]
+}).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    $(this).find('.slick-slide[data-slick-index="' + nextSlide + '"]').addClass('fuckyou');
+    $(this).find('.slick-slide[data-slick-index="' + currentSlide + '"]').removeClass('fuckyou');
+});
+3
