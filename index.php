b62102afdcee2c038c3c0f99356ac628e1ddccc0
Igor <igorhenriqueconstant@gmail.com>
2020-03-28 01:14:05 -0300

    Correções

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
     <script src="https://kit.fontawesome.com/7bc0885a91.js"></script>
 
     <!-- Slick -->
-    <link rel="stylesheet" href="assets/css/slick-theme.css">
     <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/slick-theme.css">
 </head>
 
 <body>
@@ -30,6 +30,7 @@
 
     <!-- JS -->
     <script src="assets/js/main.js" type="text/javascript"></script>
+    <script src="assets/js/slick.js"></script>
     <script src="scripts/slide.js"></script>
 </body>
 
