88355afb49eb74ca5f5af1fd2edc6ec9ddb4f0ba
Igor <igorhenriqueconstant@gmail.com>
2020-03-28 01:15:06 -0300

    Inclusão do Script

--- gulpfile.js
+++ gulpfile.js
@@ -29,8 +29,6 @@ function gulpJS() {
         'node_modules/jquery/dist/jquery.min.js',
         'node_modules/bootstrap/dist/js/bootstrap.min.js',
         'scripts/tabs.js',
-        'scripts/slick.js',
-
     ])
 
         .pipe(concat('main.js'))
